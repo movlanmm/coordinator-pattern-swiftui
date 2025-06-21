d0b80c0409ec63cb5d0b0d996a83ede3adbd7cc0
Movlan <movlan.mustafayev@gmail.com>
2025-06-21 11:07:40 +0400

    Initial Commit

---
+++ CoordinatorPattern/CoordinatorPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorPatternApp.swift
+//  CoordinatorPattern
+//
+//  Created by Movlan Mustafayev on 21.06.25.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
